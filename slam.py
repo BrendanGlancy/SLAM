3abe7781d901cc5ea55310e09cdb2130e8374071
Brendan Glancy <brendanglancy@macbook-pro.attlocal.net>
2020-09-29 19:35:06 -0400

    dinner

--- slam.py
+++ slam.py
@@ -2,21 +2,26 @@
 
 import time
 import cv2
-from display import Display
+import sdl2
+import sdl2.ext
+sdl2.ext.init()
 
 W = 1920//2
 H = 1080//2
 
-disp = Display(W, H)
-
+disp = Display(W,H)
+orb = cv2.ORB()
 
 def process_frame(img):
+    # keyPoint1 and descriptors1 detect and compute 
+    kp1, des1 = orb.detectAndCompute(img1,None)
+
     img = cv2.resize(img, (W,H))
     disp.paint(img)
 
 
 if __name__ == '__main__':
-    cap = cv2.VideoCapture("/Users/brendanglancy/Desktop/BG/twitchslam/videos/test_countryroad.mp4")
+    cap = cv2.VideoCapture("test.mp4")
 
     while cap.isOpened():
         ret, frame = cap.read()

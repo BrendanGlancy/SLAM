78aee98b91e96543cc67273691c3f9b8b916c4b2
BrendanGlancy <bglance68@gmail.com>
2020-10-07 11:39:28 -0400

    pangolin

--- slam.py
+++ slam.py
@@ -48,5 +48,28 @@ class Map(object):
         self.dcam.SetHandler(self.handler)
 
     def viewer_refresh(self, q):
-        
+        if self.state is None or not q.empty():
+            self.state = q.get()
+
+        # turn state into points
+        ppts = np.array([d[:3, 3] for d in self.state[0]])
+        spts = np.array(self.state[1])
+
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+        gl.glClearColor(1.0, 1.0, 1.0, 1.0)
+        self.dcam.Activate(self.scam)
+
+        gl.glPointSize(10)
+        gl.glColor3f(0.0, 1.0, 0.0)
+        pangolin.DrawPoints(ppts)
+
+        gl.glPointSize(2)
+        gl.glColor3f(0.0, 1.0, 0.0)
+        pangolin.DrawPoints(spts)
+
+        pangolin.FinishFrame()
+
+    def display(self):
+
+
 
